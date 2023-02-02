655b021d0d0cc680e34e2e5def9aadbf72ce85f8
Dennis Fransen <dennisfransen@hotmail.se>
2023-02-02 18:28:49 +0100

    HomeView: Added styled title, paragraphs and buttons

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,21 +1,55 @@
+<script setup lang="ts">
+import { ArrowSmallRightIcon } from '@heroicons/vue/24/solid'
+</script>
+
 <template>
 	<main class="grid lg:grid-cols-3">
 		<div class="img-container group/img">
 			<div class="img bg-anubis"></div>
 			<div class="img-inner">
-				<p>hello world</p>
+				<div>
+					<h2 class="text-5xl font-bold tracking-wide">Streamer</h2>
+					<p class="mt-2 text-lg">Do you wan't to host a tournament?</p>
+					<router-link
+						to="/"
+						class="mt-8 inline-flex items-center justify-between gap-6 rounded border px-4 py-2 text-lg"
+					>
+						Become Streamer
+						<ArrowSmallRightIcon class="h-5 w-5" />
+					</router-link>
+				</div>
 			</div>
 		</div>
 		<div class="img-container group/img">
 			<div class="img bg-inferno"></div>
 			<div class="img-inner">
-				<p>hello world</p>
+				<div>
+					<h2 class="text-5xl font-bold tracking-wide">Tournaments</h2>
+					<p class="mt-2 text-lg">Let's fight for glory and fine prices!</p>
+					<router-link
+						to="/"
+						class="mt-8 inline-flex items-center justify-between gap-6 rounded border px-4 py-2 text-lg"
+					>
+						View Tournaments
+						<ArrowSmallRightIcon class="h-5 w-5" />
+					</router-link>
+				</div>
 			</div>
 		</div>
 		<div class="img-container group/img">
 			<div class="img bg-mirage"></div>
 			<div class="img-inner">
-				<p>hello world</p>
+				<div>
+					<h2 class="text-5xl font-bold tracking-wide">About</h2>
+					<p class="mt-2 text-lg">How do i become a fears warrior?</p>
+					<router-link
+						to="/"
+						class="mt-8 inline-flex items-center justify-between gap-6 rounded border px-4 py-2 text-lg"
+					>
+						Read More
+						<ArrowSmallRightIcon class="h-5 w-5" />
+					</router-link>
+				</div>
 			</div>
 		</div>
 	</main>
@@ -23,19 +57,19 @@
 
 <style>
 .img-container {
-	@apply relative h-full w-full min-h-app overflow-hidden;
+	@apply min-h-app relative h-full w-full overflow-hidden;
 }
 
 .img-inner {
 	@apply absolute inset-0 z-20;
+	@apply grid place-items-center;
 }
 
 .img {
 	@apply absolute inset-0 overflow-hidden;
 	@apply transition-all duration-700;
 	@apply bg-cover bg-center bg-no-repeat brightness-50;
-	@apply group-hover/img:blur-none blur-md;
-	@apply group-hover/img:scale-100 scale-110;
-	@apply group-hover/img:grayscale-0 grayscale;
+	@apply blur-md group-hover/img:blur-none;
+	@apply scale-110 group-hover/img:scale-100;
 }
 </style>
