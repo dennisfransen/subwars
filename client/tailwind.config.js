79dddf61645ce96a36756f4dd014872ba75af3cf
dennisfransen <dennisfransen@hotmail.se>
2023-02-07 23:57:41 +0100

    New images

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,14 +7,28 @@ module.exports = {
 				16: '4rem',
 			},
 			backgroundImage: {
-				mirage: "url('@/assets/images/mirage.webp')",
-				anubis: "url('@/assets/images/anubis-1.webp')",
-				inferno: "url('@/assets/images/inferno.webp')",
+				smoking: "url('@/assets/images/smoking.jpg')",
+				csgo: "url('@/assets/images/csgo.jpeg')",
+
+				't-red': "url('@/assets/images/t-red.jpg')",
+				't-green': "url('@/assets/images/t-green.jpg')",
+				'ct-green': "url('@/assets/images/ct-green.jpg')",
+
 				crowd: "url('@/assets/images/crowd.jpg')",
 				streaming: "url('@/assets/images/streaming.jpg')",
 				keyboard: "url('@/assets/images/keyboard.jpg')",
-				smoking: "url('@/assets/images/smoking.jpg')",
-				csgo: "url('@/assets/images/csgo.jpeg')",
+				question: "url('@/assets/images/question.jpg')",
+				'half-keyboard': "url('@/assets/images/half-keyboard.jpg')",
+				mic: "url('@/assets/images/mic.jpg')",
+				computer: "url('@/assets/images/computer.jpg')",
+
+				'crowd-min': "url('@/assets/images/crowd-min.jpg')",
+				'streaming-min': "url('@/assets/images/streaming-min.jpg')",
+				'keyboard-min': "url('@/assets/images/keyboard-min.jpg')",
+				'question-min': "url('@/assets/images/question-min.jpg')",
+				'half-keyboard-min': "url('@/assets/images/half-keyboard-min.jpg')",
+				'mic-min': "url('@/assets/images/mic-min.jpg')",
+				'computer-min': "url('@/assets/images/computer-min.jpg')",
 			},
 			fontFamily: {
 				graduate: 'Graduate, cursive',
